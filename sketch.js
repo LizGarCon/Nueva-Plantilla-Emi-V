51682a77a437dfdbb6ab9bfe1b1a298d508d2a85
LizGarCon <89824155+LizGarCon@users.noreply.github.com>
2022-03-12 12:27:28 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function preload() {
+
+}
+
+
+function setup() {
+    createCanvas(2000, 1200);
+
+}
+
+function draw() {
+    edges = createEdgeSprites();
+ 
+    background("black");
+
+   
+    // Agregar el código para Cambio de estado a END
+    drawSprites();
+}
